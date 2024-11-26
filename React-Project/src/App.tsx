4af9c6ce09893318cc8a19c41a452f5581bfbd6b
umesh-bingi <bingiumesh.13@gmail.com>
2024-11-26 21:36:25 +0530

    Day-4
    React Setup With TypeScript and Bootstrap

---
+++ React-Project/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import Message from './Message'
+import ListGrop from "./components/ListGrop";
+const App = () => {
+  return (
+    <div>
+      {/* <Message/> */}
+      <ListGrop />
+    </div>
+  );
+};
+
+export default App;
