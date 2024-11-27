3bf5a21164b129c04ffeb6418304894b893035ff
umesh-bingi <bingiumesh.13@gmail.com>
2024-11-27 18:54:19 +0530

    Basics of React
    covered topics like How to array using props , rendering arrays ,conditions and adding colors dynamically on conditions.

--- React-basics-JS/src/Product.jsx
+++ React-basics-JS/src/Product.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import "./Product.css";
 
-const Product = ({ title, price }) => {
+const Product = ({ title, price, features }) => {
+  // const list = features.map((feature) => <li>{feature}</li>);
+  let styles = { backgroundColor: price > 30000 && "yellow" };
   return (
-    <div className="Product">
+    <div className="Product" style={styles}>
       <h3>{title}</h3>
       <h5>Price :{price}</h5>
+      {/* <p>{list}</p> */}
+      {/* <p>
+        {features.map((feature) => (
+          <li>{feature}</li>
+        ))}
+      </p> */}
+
+      {/* write a condition if price is greater than 30000 it should display get 5% discount and also bg color yellow*/}
+      {price > 30000 && <p>Get 5% discount</p>}
     </div>
   );
 };
--- React-basics-JS/src/ProductList.jsx
+++ React-basics-JS/src/ProductList.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Product from "./Product";
 
 const ProductList = () => {
+  let list1 = ["hi", "hello", "bye"];
   return (
     <>
-      <Product title="Phones" price={10000} />
+      {/* <Product title="Phones" price={10000} features={list1} /> */}
+      <Product title="Phones" price={10000} features={list1} />
       <Product title="Laptop" price={100000} />
       <Product title="Ipad" price={25000} />
     </>
