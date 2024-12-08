9a390d0f86aae77ed9f3eb59ebf64fd949933e6f
BingiUmesh <bingiumesh.13@gmail.com>
2024-12-08 12:27:00 +0530

    Implemented Layouts and Route Group Layouts

---
+++ Next.Js/sample-app/src/app/(auth)/(auth-layout)/layout.tsx
@@ -0,0 +1,12 @@
+export default function ProdctDetailsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <p>Auth Layout</p>
+      {children}
+    </>
+  );
+}
---
+++ Next.Js/sample-app/src/app/products/[productId]/layout.tsx
@@ -0,0 +1,12 @@
+export default function ProdctDetailsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      {children}
+      <p>Featured products caresoul</p>
+    </>
+  );
+}
