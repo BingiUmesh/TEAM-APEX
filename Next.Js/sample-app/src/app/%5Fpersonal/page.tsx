26c5259fc722c3988902fdf04ee991b6ed2184de
BingiUmesh <bingiumesh.13@gmail.com>
2024-12-08 12:01:28 +0530

    Implement Private Folders and Router Groups

---
+++ Next.Js/sample-app/src/app/%5Fpersonal/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>you can see me</div>;
+};
+
+export default page;
--- Next.Js/sample-app/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ Next.Js/sample-app/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-
+import { notFound } from "next/navigation";
 const reviewId = ({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) => {
+  if (parseInt(params.reviewId) > 1000) notFound();
+
   return (
     <div>
       <h2>Product id {params.productId}</h2>
