6ef35b1cabd11fa77ebf48ac8199bed846cd588b
BingiUmesh <bingiumesh.13@gmail.com>
2024-12-08 10:36:57 +0530

    Complete Basics of Next.js and Routing Concepts
    Covered the foundational concepts of Next.js, including its core features, routing conventions and implemented routing, and nested routing.

---
+++ Next.Js/sample-app/src/app/blog/first/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const fstBlog = () => {
+  return (
+    <div>
+      <h2>first Blog</h2>
+    </div>
+  );
+};
+
+export default fstBlog;
---
+++ Next.Js/sample-app/src/app/blog/second/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const secBlog = () => {
+  return (
+    <div>
+      <h2>second Blog</h2>
+    </div>
+  );
+};
+
+export default secBlog;
