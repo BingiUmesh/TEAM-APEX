e36988b6257e42108a23c9ca4edd443f8d0a178f
BingiUmesh <bingiumesh.13@gmail.com>
2024-12-12 07:37:49 +0530

    Loading & Error Handling ,Recovering from Error

---
+++ Next.Js/sample-app/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+
+const ErrorBoundary = ({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) => {
+  return (
+    <div>
+      {error.message}
+      <button onClick={reset}>try again</button>
+    </div>
+  );
+};
+
+export default ErrorBoundary;
